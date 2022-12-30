db5da39cd6751bda4000c52582b0c80e0a652105
sheeraz-kalhoro <sheeraz@outlook.com>
2022-12-30 12:06:07 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Hello from './Hello'
+
+const App = () => {
+  return (
+    <div>
+        <Hello/>
+        <h1>Welcome to the Minimal Reat</h1>
+    </div>
+  )
+}
+
+export default App
